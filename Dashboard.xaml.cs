e548c3bbe7f959c2c6a4f0a6d5788da9bc46da91
Ali Haider <ali74576@gmail.com>
2020-09-21 09:40:32 +0500

    Add project files.

---
+++ Dashboard.xaml.cs
@@ -0,0 +1,98 @@
+﻿using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Social_Blade_Dashboard
+{
+    /// <summary>
+    /// Interaction logic for Dashboard.xaml
+    /// </summary>
+    public partial class Dashboard : UserControl
+    {
+        public SeriesCollection SeriesCollection { get; set; }
+        public SeriesCollection LastHourSeries { get; set; }
+        public SeriesCollection LastHourSeries1 { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double,string> Formatter { get; set; }
+        public Dashboard()
+        {
+            InitializeComponent();
+            SeriesCollection = new SeriesCollection
+            {
+                new StackedColumnSeries
+                {
+                    Values = new ChartValues<double> {25,52,61,89},
+                    StackMode = StackMode.Values,
+                    DataLabels = true
+                },
+                 new StackedColumnSeries
+                {
+                    Values = new ChartValues<double> {-15,-75,-16,-49},
+                    StackMode = StackMode.Values,
+                    DataLabels = true
+                }
+            };
+            LastHourSeries = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    AreaLimit = -10,
+                    Values = new ChartValues<ObservableValue>
+                    {
+                        new ObservableValue(3),
+                        new ObservableValue(1),
+                        new ObservableValue(9),
+                        new ObservableValue(4),
+                        new ObservableValue(5),
+                        new ObservableValue(3),
+                        new ObservableValue(1),
+                        new ObservableValue(2),
+                        new ObservableValue(3),
+                        new ObservableValue(7),
+                    }
+                }
+            };
+            LastHourSeries1 = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    AreaLimit = -10,
+                    Values = new ChartValues<ObservableValue>
+                    {
+                        new ObservableValue(13),
+                        new ObservableValue(11),
+                        new ObservableValue(9),
+                        new ObservableValue(14),
+                        new ObservableValue(5),
+                        new ObservableValue(3),
+                        new ObservableValue(12),
+                        new ObservableValue(2),
+                        new ObservableValue(3),
+                        new ObservableValue(7),
+                    }
+                }
+            };
+            Labels = new[] { "Feb 7", "Feb 8", "Feb 9", "Feb 10" };
+            Formatter = value => value.ToString();
+            DataContext = this;
+            string imgCartoon = $"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString()}\\Images\\cartoon-woman-pretty.png";
+            string imgavatar = $"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString()}\\Images\\avatar1.jpg";
+            ImgCartoon.Source = new BitmapImage(new Uri(imgCartoon));
+            avatar1.Source = new BitmapImage(new Uri(imgavatar));
+            avatar2.Source = new BitmapImage(new Uri(imgavatar));
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Social_Blade_Dashboard
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            RenderPages.Children.Clear();
+            RenderPages.Children.Add(new Dashboard());
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
